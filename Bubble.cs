3bdd008b109d3cdb40f03f2391f3a6ec05e3d3c1
TrinityHaig <000915272@mohawkcollege.ca>
2025-03-08 16:21:03 -0500

    getting a weird error in the bubble class

--- Bubble.cs
+++ Bubble.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,9 +10,20 @@ namespace lastname_firstname_template_v1_3_3
 {
     internal class Bubble
     {
-       public Bubble()
+        Vector2 position;
+        Vector2 direction;
+        float speed;
+        float radius;
+        Color colour;
+        bool atEdgeOfScreen;
+
+       public Bubble(Vector2 pos, float r, Color c)
         {
-            
+            position = pos;
+            radius = r;
+            colour = c;
+            //Random direction
+            direction = Random.Direction; //why am i getting an error.
         }
     }
 }
