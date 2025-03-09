3e14cec4fd0cd5ca341b2ff6a2bb61506b4138a7
TrinityHaig <000915272@mohawkcollege.ca>
2025-03-08 20:00:21 -0500

    started player class stuff

--- Player.cs
+++ Player.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using MohawkGame2D;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,19 @@ namespace lastname_firstname_template_v1_3_3
 {
     internal class Player
     {
+        public Vector2 position;
+        public float hitbox = 0;
+        public Vector2 size;
         public Player()
         {
 
+        }
+        public void Render()
+        {
+            Draw.FillColor = Color.Black;
+            Draw.Circle(position, 15);
+            Draw.FillColor = Color.Black;
+            Draw.Rectangle(position,size);
         }
     }
 }
